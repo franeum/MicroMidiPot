d50bacdeb04df9540c3805d69c37396998def963
franeum <elettrochuck@gmail.com>
2023-11-10 17:47:52 +0100

    add a pot

--- examples/array/array.ino
+++ examples/array/array.ino
@@ -1,20 +1,21 @@
 #include <MicroMidiPot.h>
 
 #define MIDI_CHANNEL 0
+#define NUMBER 3
 
-const int PIN_POTS[] = {A0, A1};
-const int N_CONTROLLERS[] = {50, 100};
+const int PIN_POTS[] = {A0, A1, A2};
+const int N_CONTROLLERS[] = {50, 51, 52};
 
-MicroMidiPot *pot[2];
+MicroMidiPot *pot[NUMBER];
 
 void setup()
 {
-  for (int i = 0; i < 2; i++)
+  for (int i = 0; i < NUMBER; i++)
     pot[i] = new MicroMidiPot(PIN_POTS[i], MIDI_CHANNEL, N_CONTROLLERS[i], "POT " + String(i));
 }
 
 void loop()
 {
-  for (int i = 0; i < 2; i++)
+  for (int i = 0; i < NUMBER; i++)
     pot[i]->update();
 }
