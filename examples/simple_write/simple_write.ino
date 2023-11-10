54f23cc330fdb226cb7b00c77623c3faf600702a
franeum <elettrochuck@gmail.com>
2023-11-10 15:39:27 +0100

    clean example

--- examples/simple_write/simple_write.ino
+++ examples/simple_write/simple_write.ino
@@ -1,4 +1,3 @@
-// include the ResponsiveAnalogRead library
 #include <MicroMidiPot.h>
 
 #define PINPOT1 A0;
@@ -8,7 +7,6 @@ MicroMidiPot pot1, pot2;
 
 void setup()
 {
-  // begin serial so we can see analog read values through the serial monitor
   pot1.begin(PINPOT1, 0, 50);
   pot2.begin(PINPOT2, 0, 100, "POT2");
   Serial.begin(115200);
